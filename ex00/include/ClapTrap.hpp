3c0dda35bf95771cb0272a9e4d4baea3dc36168f
Benjamin BRASSART <bbrassar@student.42.fr>
2022-04-26 19:34:11 +0200

    Added some fancy stuff Added ClapTrap implementation Added tests

--- ex00/include/ClapTrap.hpp
+++ ex00/include/ClapTrap.hpp
@@ -6,18 +6,28 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 17:58:26 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/04/26 18:10:27 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/04/26 19:29:15 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLAPTRAP_HPP
 # define CLAPTRAP_HPP
 
+# include <ostream>
 # include <string>
 
 class ClapTrap
 {
 	private:
+	static std::string const	DEFAULT_NAME;
+	static std::string const	PREFIX;
+	static std::string const	RED;
+	static std::string const	GREEN;
+	static std::string const	BLUE;
+	static std::string const	RESET;
+
+	private:
+	std::string		name;
 	unsigned int	hitPoints;
 	unsigned int	energyPoints;
 	unsigned int	attackDamage;
@@ -25,14 +35,15 @@ class ClapTrap
 	public:
 	ClapTrap(void);
 	ClapTrap(ClapTrap const &x);
+	ClapTrap(std::string const &name);
 	~ClapTrap();
 
-	ClapTrap	&operator=(ClapTrap const &x);
+	ClapTrap			&operator=(ClapTrap const &x);
 
 	public:
-	void		attack(std::string const &target);
-	void		takeDamage(unsigned int amount);
-	void		beRepaired(unsigned int amount);
+	void				attack(std::string const &target);
+	void				takeDamage(unsigned int amount);
+	void				beRepaired(unsigned int amount);
 }; // class ClapTrap
 
 #endif // CLAPTRAP_HPP

7cb4bb6b1010c345ccaf03a56c5cf4657799275d
Benjamin BRASSART <bbrassar@student.42.fr>
2022-04-28 20:49:14 +0200

    Added tests

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,12 +6,12 @@
 /*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 18:29:22 by bbrassar          #+#    #+#             */
-/*   Updated: 2022/04/26 19:32:47 by bbrassar         ###   ########.fr       */
+/*   Updated: 2022/04/28 20:44:55 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../tests.h"
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
 
 static void	_constructor_default0(void);
 static void	_constructor_default1(void);
@@ -26,6 +26,7 @@ static void	_repair2(void);
 static void	_damage0(void);
 static void	_damage1(void);
 static void	_damage2(void);
+static void	_guard(void);
 
 int	main(void)
 {
@@ -43,43 +44,44 @@ int	main(void)
 		TEST(_damage0)
 		TEST(_damage1)
 		TEST(_damage2)
+		TEST(_guard)
 	};
 	TEST_RUN();
 }
 
 static void	_constructor_default0(void)
 {
-	ClapTrap();
+	ScavTrap();
 }
 
 static void	_constructor_default1(void)
 {
-	ClapTrap("coucou");
+	ScavTrap("coucou");
 }
 
 static void	_constructor_copy(void)
 {
-	ClapTrap	c("bonjour");
-	ClapTrap	copy(c);
+	ScavTrap	c("bonjour");
+	ScavTrap	copy(c);
 }
 
 static void	_operator_copy(void)
 {
-	ClapTrap	c;
+	ScavTrap	c;
 
-	c = ClapTrap("salut");
+	c = ScavTrap("salut");
 }
 
 static void	_attack0(void)
 {
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 
 	c.attack("123");
 }
 
 static void	_attack1(void)
 {
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 
 	c.attack("123");
 	c.attack("123");
@@ -89,21 +91,21 @@ static void	_attack1(void)
 
 static void	_attack2(void)
 {
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 	for (int i = 0; i < 30; ++i)
 		c.attack("123");
 }
 
 static void	_repair0(void)
 {
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 
 	c.beRepaired(12);
 }
 
 static void	_repair1(void)
 {
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 
 	c.beRepaired(12);
 	c.beRepaired(15);
@@ -112,7 +114,7 @@ static void	_repair1(void)
 
 static void	_repair2(void)
 {
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 
 	c.beRepaired(12);
 	c.takeDamage(44);
@@ -123,7 +125,7 @@ static void	_repair2(void)
 static void	_damage0(void)
 {
 
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 
 	c.takeDamage(1);
 }
@@ -131,7 +133,7 @@ static void	_damage0(void)
 static void	_damage1(void)
 {
 
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 
 	c.takeDamage(1);
 	c.takeDamage(2);
@@ -142,8 +144,15 @@ static void	_damage1(void)
 static void	_damage2(void)
 {
 
-	ClapTrap c("hello");
+	ScavTrap c("hello");
 
 	c.takeDamage(44);
 	c.takeDamage(44);
+}
+
+static void	_guard(void)
+{
+	ScavTrap c("hai");
+
+	c.guardGate();
 }
